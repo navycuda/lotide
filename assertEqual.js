44f922b7e6af58e02e11b4eb070024013392decd
shane <navysupra@gmail.com>
2022-07-25 22:01:08 +0000

    Smiley faces for passed, dissatisfied face for failed

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// Functions
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log(`😆 Assertion Passed: ${actual} === ${expected}`);
+  else
+    console.log(`🤬 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// Testing
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("USS Enterprise", "USS Enterprise");
+assertEqual("1701", "1701-A");
+assertEqual((6 % 2), (278 % 2));
+assertEqual((6 % 2), (279 % 2));

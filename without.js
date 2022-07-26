ef1d6f621c4ef813ca2e36a1f924df5893dfaecd
shane <navysupra@gmail.com>
2022-07-26 23:24:15 +0000

    without works, tried a few different test cases

--- without.js
+++ without.js
@@ -12,5 +12,39 @@ const assertArraysEqual = function(first, second) {
     console.log(`🤬 Assertion Failed: ${first} !== ${second}`);
   }
 };
+const without = function(source, itemsToRemove) {
+  let result = [];
+
+  for (let s = 0; s < source.length; s++) {
+    let exists = false;
+    for (let i = 0; i < itemsToRemove.length; i++) {
+      if (eqArrays(source[s], itemsToRemove[i])) {
+        exists = true;
+      }
+    }
+    if (!exists) {
+      result.push(source[s]);
+    }
+  }
+
+  return result;
+};
 
 // Execution
+const words = [ "hello", "world", "lighthouse" ];
+const shortWords = without(words, ["lighthouse"]);
+
+console.log(shortWords);
+
+assertArraysEqual(words, [ "hello", "world", "lighthouse" ]);
+assertArraysEqual(shortWords, [ "hello", "world", "lighthouse" ]);
+
+const calEZ45C = [ "23701EZ45C.XCAL", "bcx", "0x80020000", 4 ];
+const cal9FU5C = [ "237019FU5C.XCAL", "bcx", "0x80020000", 4 ];
+
+const shortCalEZ45C = without(calEZ45C, [ "bcx" ]);
+const shortCal9FU5C = without(cal9FU5C, [ "bcx" ]);
+
+assertArraysEqual(calEZ45C, shortCalEZ45C);
+assertArraysEqual(cal9FU5C, shortCal9FU5C);
+assertArraysEqual(shortCalEZ45C, shortCal9FU5C);

bd76dbfc6137a72032ecc8f30774b94fde54453b
shane <navysupra@gmail.com>
2022-07-27 23:57:12 +0000

    Added copy&paste and basic regions

---
+++ findKeyByValue.js
@@ -0,0 +1,11 @@
+// Copy&Paste Functions
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log(`😆 Assertion Passed: ${actual} === ${expected}`);
+  else
+    console.log(`🤬 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// Functions
+
+// Execution

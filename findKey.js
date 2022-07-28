c93ba6cd98a98ea04cfbb59b047b7ef706123571
shane <navysupra@gmail.com>
2022-07-28 23:01:40 +0000

    Basic function working, adding more test cases

--- findKey.js
+++ findKey.js
@@ -8,7 +8,19 @@ const assertEqual = function(actual, expected) {
 
 // Local Functions
 const findKey = function (object, callback) {
-
+  
+  for (const [key, value] of Object.entries(object)) {
+    if (callback(value))
+      return key;
+  }
 };
 
 // Execution & Test Data
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), `noma`);

99faf3ab6bf3e0663485f1fbf09711dd5cc98999
shane <navysupra@gmail.com>
2022-07-28 00:10:18 +0000

    searches for key by value

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -7,5 +7,20 @@ const assertEqual = function(actual, expected) {
 };
 
 // Functions
+const findKeyByValue = function(obj, searchValue) {
+  for (const [key, value] of Object.entries(obj)) {
+    if (value === searchValue) {
+      return key;
+    }
+  }
+};
 
 // Execution
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

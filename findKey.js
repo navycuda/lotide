9a617fcaadfffdc8226f42893f2cda39ec525f0a
shane <navysupra@gmail.com>
2022-07-28 22:54:31 +0000

    copy&paste assertEqual, prep file

---
+++ findKey.js
@@ -0,0 +1,14 @@
+// Global Functions
+const assertEqual = function(actual, expected) {
+  if (actual === expected)
+    console.log(`😆 Assertion Passed: ${actual} === ${expected}`);
+  else
+    console.log(`🤬 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// Local Functions
+const findKey = function (object, callback) {
+
+};
+
+// Execution & Test Data

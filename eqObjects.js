acfc22c692a35064652086f9eaaf8e37fac3a924
shane <navysupra@gmail.com>
2022-07-28 01:40:05 +0000

    adding more methods to the object

--- eqObjects.js
+++ eqObjects.js
@@ -9,29 +9,39 @@ const assertEqual = function(actual, expected) {
 // Functions
 // Returns true if both objects identical.
 const eqObjects = function(obj1, obj2) {
+  // Create a detailed object of the object to simplify looping
+  // later.
   const getObjDetails = function(obj) {
     return {
+      orginal: obj,
       keyCount: Object.keys(obj).length,
-      
+      hasKeyValuePair: function(key, value) {
+        return obj[key] ? obj[key] === value : false;
+      }
     };
   };
   console.log(obj1);
   console.log(obj2);
 
-
+  // Detail the input objects
   const object1 = getObjDetails(obj1);
   const object2 = getObjDetails(obj2);
 
   console.log(object1);
   console.log(object2);
 
-
+  // Test the objects
   if (object1.keyCount !== object2.keyCount) {
     return false;
   }
-
-
-  
+  for (const [key, value] of Object.entries(object1.orginal)) {
+    if (!object2.hasKeyValuePair(key,value))
+      return false;
+  }
+  for (const [key, value] of Object.entries(object2.orginal)) {
+    if (!object1.hasKeyValuePair(key,value))
+      return false;
+  }
   return true;
 };
 

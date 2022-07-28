9873622205592c34572d0b5db58aa83e8b966f7e
shane <navysupra@gmail.com>
2022-07-28 02:15:16 +0000

    Now able to compare values and check arrays, I might be able to implemenent recursively

--- eqObjects.js
+++ eqObjects.js
@@ -5,8 +5,29 @@ const assertEqual = function(actual, expected) {
   else
     console.log(`🤬 Assertion Failed: ${actual} !== ${expected}`);
 };
+// Using the orginal eqArrays to be more in keeping with the
+// spirit of the excersize.
+const eqArrays = function(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
+};
 
 // Functions
+// used to filter through the different possible data types
+// within the scope of the project.
+const compareValues = function(value1, value2) {
+  if (Array.isArray(value1) && Array.isArray(value2)) {
+    return eqArrays(value1,value2);
+  }
+  return value1 === value2;
+};
 // Tests entries
 
 // Returns true if both objects identical.
@@ -18,7 +39,7 @@ const eqObjects = function(obj1, obj2) {
       orginal: obj,
       keyCount: Object.keys(obj).length,
       hasKeyValuePair: function(key, value) {
-        return this.orginal[key] ? this.orginal[key] === value : false;
+        return this.orginal[key] ? compareValues(this.orginal[key], value) : false;
       },
       equals: function(obj) {
         let count = 0;
@@ -52,3 +73,11 @@ const abc = { a: '1', b: '2', c: '3' };
 
 assertEqual(eqObjects(ab,ba), true);
 assertEqual(eqObjects(ab,abc), false);
+
+// Step 3 assertions
+const cd = { c: "1", d: [ "2", 3 ] };
+const dc = { d: [ "2" , 3], c: "1" };
+const cd2 = { c: "1", d: [ "2", 3, 4 ]};
+
+assertEqual(eqObjects(cd,dc), true);
+assertEqual(eqObjects(cd,cd2), false);

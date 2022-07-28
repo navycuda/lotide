75006dbe239a65af59cb941ad0973b58fd7e18a2
shane <navysupra@gmail.com>
2022-07-28 21:29:12 +0000

    added empty map function

--- map.js
+++ map.js
@@ -2,7 +2,13 @@
 
 
 // Local Functions
+const map = function() {
 
+};
 
 // Execution & Test Data
 const starships = [ `USS Enterprise`, `USS Reliant`, `USS Excelsior`, `USS Yamato`, `USS Lexington`, `USS Intrepid` ];
+
+const results1 = map(starships, uss => uss[0]);
+
+console.log(results1);

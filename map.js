67299c1bfe5ddb0f5dc558fbb251335ae6c658fa
shane <navysupra@gmail.com>
2022-07-28 21:25:17 +0000

    Added test data

---
+++ map.js
@@ -0,0 +1,8 @@
+// Global Functions
+
+
+// Local Functions
+
+
+// Execution & Test Data
+const starships = [ `USS Enterprise`, `USS Reliant`, `USS Excelsior`, `USS Yamato`, `USS Lexington`, `USS Intrepid` ];

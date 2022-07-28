04a402cf0e8858f4e96019eabe63d15e82525ff9
shane <navysupra@gmail.com>
2022-07-28 20:58:09 +0000

    shorten commenting, made easier for copy&paste

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,5 @@
-// Functions
+// Copy&Paste Functions
 const eqArrays = function(first, second) {
-  // first tried toString(), then submitted loop for previous attempt
-  // I looked into serializing the arrays so I can make a perfect
-  // comparison of them regardless data types within the array.
   return JSON.stringify(first) === JSON.stringify(second);
 };
 const assertArraysEqual = function(first, second) {

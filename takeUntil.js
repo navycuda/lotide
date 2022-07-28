c29f449fadf596d7828d98acccfb784b1a366942
shane <navysupra@gmail.com>
2022-07-28 22:51:13 +0000

    passed testing

--- takeUntil.js
+++ takeUntil.js
@@ -27,6 +27,9 @@ const takeUntil = function(array, callback) {
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const correct1 = [ 1, 2, 5, 7, 2 ];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
 assertArraysEqual(results1,correct1);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const correct2 = [ 'I\'ve', 'been', 'to', 'Hollywood' ];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, correct2);

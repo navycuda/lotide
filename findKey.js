b0419fe6e56963918b4f78e80f8de21811f54987
shane <navysupra@gmail.com>
2022-07-31 07:27:50 +0000

    I can't find any difference... not sure what I did

--- findKey.js
+++ findKey.js
@@ -8,7 +8,6 @@ const assertEqual = function(actual, expected) {
 
 // Local Functions
 const findKey = function (object, callback) {
-  
   for (const [key, value] of Object.entries(object)) {
     if (callback(value))
       return key;

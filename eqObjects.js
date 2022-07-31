abb8e0d09ada856106b3738f4e282f0a75eeca20
shane <navysupra@gmail.com>
2022-07-31 08:23:30 +0000

    Added test cases for nested arrays

--- eqObjects.js
+++ eqObjects.js
@@ -65,6 +65,10 @@ const light0 = [{ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }];
 const light1 = [{ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }];
 const light2 = [{ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }];
 
+const nested0 = [[[2, 3], [4]], [[2, 3], [4]]];
+const nested1 = [[[2, 3], [4]], [[2, 3], [4, 5]]];
+const nested2 = [[[2, 3], [4]], [[2, 3], 4]];
+
 const object0 = {
   name: 'object0',
   id: 0x80020000,
@@ -109,3 +113,9 @@ assertEqual(eqObjects(object0, object00), true);
 assertEqual(eqObjects(object0, object1), false);
 assertEqual(eqObjects(object0, object2), false);
 assertEqual(eqObjects(object0, object3), false);
+
+console.log(`nested arrays`);
+assertEqual(eqArrays(nested0[0], nested0[1]), true);
+assertEqual(eqArrays(nested1[0], nested1[1]), false);
+assertEqual(eqArrays(nested2[0], nested2[1]), false);
+

c3bee5f9cf50d86d39c9aa42ad12d6f7d679b828
Shane <navysupra@gmail.com>
2022-08-01 23:58:45 +0000

    modifed head to use require

--- head.js
+++ head.js
@@ -1,10 +1,5 @@
 // Functions
-const assertEqual = function(actual, expected) {
-  if (actual === expected)
-    console.log(`😆 Assertion Passed: ${actual} === ${expected}`);
-  else
-    console.log(`🤬 Assertion Failed: ${actual} !== ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 const head = function(array) {
   return array[0];
 };

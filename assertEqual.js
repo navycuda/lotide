0e3f2759ff4765a395c77736d19c8c7a23afbafc
Shane <navysupra@gmail.com>
2022-08-01 23:58:59 +0000

    commented out the console.log, added exports

--- assertEqual.js
+++ assertEqual.js
@@ -7,9 +7,12 @@ const assertEqual = function(actual, expected) {
 };
 
 // Testing
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("USS Enterprise", "USS Enterprise");
-assertEqual("1701", "1701-A");
-assertEqual((6 % 2), (278 % 2));
-assertEqual((6 % 2), (279 % 2));
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual("USS Enterprise", "USS Enterprise");
+// assertEqual("1701", "1701-A");
+// assertEqual((6 % 2), (278 % 2));
+// assertEqual((6 % 2), (279 % 2));
+
+// Exports
+module.exports = assertEqual;

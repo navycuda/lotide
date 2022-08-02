73c64f8c4c1d98e02b9cf3a93c93983558e71cf4
Shane <navysupra@gmail.com>
2022-08-02 05:35:47 +0000

    updated requires for new paths

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 // index
-const head = require(`./head`);
-const tail = require(`./tail`);
-const middle = require(`./middle`);
+// Head, returns first element of an array
+const head = require(`./modules/head`);
+// Tail, returns all but the first element
+const tail = require(`./modules/tail`);
+// Returns an array of the middle 1 or 2 elements of the array
+const middle = require(`./modules/middle`);
+
+
+
 
 // Exports
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle
+  head,
+  tail,
+  middle
 };

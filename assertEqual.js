81374e1bc8025d531e133b858562cd1b83df2cce
Shane <navysupra@gmail.com>
2022-08-02 00:19:35 +0000

    minor cleanup

--- assertEqual.js
+++ assertEqual.js
@@ -6,13 +6,5 @@ const assertEqual = function(actual, expected) {
     console.log(`🤬 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// Testing
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual("USS Enterprise", "USS Enterprise");
-// assertEqual("1701", "1701-A");
-// assertEqual((6 % 2), (278 % 2));
-// assertEqual((6 % 2), (279 % 2));
-
 // Exports
 module.exports = assertEqual;

e7341659abf36aa74a9c2d9872a11f33bdabef98
Shane <navysupra@gmail.com>
2022-08-02 02:53:48 +0000

    made no changes, unsure if should be working on this file

--- test/assertArrayEqualsTest.js
+++ test/assertArrayEqualsTest.js
@@ -2,6 +2,7 @@
 const assert = require(`chai`).assert;
 const assertArraysEqual = require(`../assertArraysEqual`);
 
+
 // Testing parameters
 assertArraysEqual([ "23701EZ45C.XCAL", "bcx", "0x80020000", 4 ], [ "23701EZ45C.XCAL", "bcx", "0x80020000", 4]);
 assertArraysEqual([ "23701EZ45C.XCAL", "bcx", "0x80020000", 4 ], [ "237019FU5C.XCAL", "bcx", "0x80020000", 4]);

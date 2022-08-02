dd5c26b2d3c9c2b642a54dd3b09b0815a2be305a
Shane <navysupra@gmail.com>
2022-08-02 03:49:13 +0000

    Made a function to translate old tests to mocha/chai.  Hope it doesn't violate the spirit of the excersize.

--- test/middleTests.js
+++ test/middleTests.js
@@ -1,8 +1,21 @@
 // Require
-const assertArraysEqual = require(`../assertArraysEqual`);
+const assert = require(`chai`).assert;
 const middle = require(`../middle`);
+const eqArrays = require(`../eqArrays`);
 
 // Testing
+const description = `middle()`;
+const assertions = [];
+
+
+const assertArraysEqual = function(actual, expected) {
+  const assertion = {};
+  assertion.description = `${actual.toString()} middle value is ${expected.toString()}`
+  assertion.actual = eqArrays(actual, expected);
+  assertion.expected = true;
+  assertions.push(assertion);
+};
+
 assertArraysEqual(middle([1]), []);
 assertArraysEqual(middle([1,2]), []);
 assertArraysEqual(middle([1,2,3]), [2]);
@@ -13,3 +26,13 @@ assertArraysEqual(middle([1,2,3,4,5,6,7]), [4]);
 assertArraysEqual(middle([1,2,3,4,5,6,7,8]), [4,5]);
 assertArraysEqual(middle([1,2,3,4,5,6,7,8,9]), [5]);
 assertArraysEqual(middle([1,2,3,4,5,6,7,8,9,0]), [5,6]);
+
+// console.log(assertions);
+
+describe(description, () => {
+  for (const assertion of assertions) {
+    it(assertion.description, () => {
+      assert.strictEqual(assertion.actual, assertion.expected);
+    });
+  }
+});

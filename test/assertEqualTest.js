94b4084d6567df4845c558848c8d37b5d0796fa6
Shane <navysupra@gmail.com>
2022-08-02 00:12:40 +0000

    Move assertion tests to testfile

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+// Testing
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("USS Enterprise", "USS Enterprise");
+assertEqual("1701", "1701-A");
+assertEqual((6 % 2), (278 % 2));
+assertEqual((6 % 2), (279 % 2));

510b9870b4426270bda2571e7cc9a64ddceda7d1
Shane <navysupra@gmail.com>
2022-08-02 04:00:19 +0000

    refactored by function to mocha/chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,18 @@
 // Required
+const assert = require(`chai`).assert;
 const tail = require(`../tail`);
-const assertEqual = require(`../assertEqual`);
 
+const description = `tail()`;
+const assertions = [];
+
+// Cheat my way into test cases fitting mocha/chai
+const assertEqual = function (actual, expected) {
+  const assertion = {};
+  assertion.description = `${actual.toString()} middle value is ${expected.toString()}`
+  assertion.actual = actual === expected;
+  assertion.expected = true;
+  assertions.push(assertion);
+};
 
 // Test the function of tail
 const result = tail(["Hello", "Lighthouse", "Labs"]);
@@ -14,3 +25,12 @@ assertEqual(result[1], "Labs");
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
 assertEqual(words.length, 3);
+
+// output for mocha/chai
+describe(description, () => {
+  for (const assertion of assertions) {
+    it(assertion.description, () => {
+      assert.strictEqual(assertion.actual, assertion.expected);
+    });
+  }
+});

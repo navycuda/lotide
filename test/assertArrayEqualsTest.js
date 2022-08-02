b22b719817cb0f58af185f9bda9743ba2533e9b9
Shane <navysupra@gmail.com>
2022-08-02 01:26:00 +0000

    Move assertArraysEqual tests into test file, export assertArrayEquals

---
+++ test/assertArrayEqualsTest.js
@@ -0,0 +1,6 @@
+// Required
+const assertArraysEqual = require(`../assertArraysEqual`);
+
+// Testing parameters
+assertArraysEqual([ "23701EZ45C.XCAL", "bcx", "0x80020000", 4 ], [ "23701EZ45C.XCAL", "bcx", "0x80020000", 4]);
+assertArraysEqual([ "23701EZ45C.XCAL", "bcx", "0x80020000", 4 ], [ "237019FU5C.XCAL", "bcx", "0x80020000", 4]);

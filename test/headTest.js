f8c2fd14077c434880bfdfd41575ad525e4ad3d4
Shane <navysupra@gmail.com>
2022-08-02 02:26:10 +0000

    implement the first test of an array

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,14 @@
 // Required
-const assertEqual = require(`../assertEqual`);
+// const assertEqual = require(`../assertEqual`);
+const assert = require(`chai`).assert;
 const head = require(`../head`);
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe(`#head`, () => {
+  it(`returns 5 for [5, 6, 7]`, () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+});
+
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

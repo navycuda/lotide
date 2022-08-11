b468ef9283a3a81388d46d72cd9cb269d2984510
Shane <navysupra@gmail.com>
2022-08-11 18:46:57 +0000

    Building tests for circular numbers

---
+++ test/circularNumbersTest.js
@@ -0,0 +1,11 @@
+const assert = require('chai');
+const circularNumbers = require('../modules/circularNumbers');
+
+describe('circularNumbers(position, limit)', () => {
+  it('limit : 10, position 7 to 8 ', () => {
+    
+
+
+
+  });
+});

ed6a6e781371e2c90d32dc5ade1755bf5bb4fc96
Shane <navysupra@gmail.com>
2022-08-15 16:47:12 +0000

    got circularNumbers working

--- modules/circularNumbers.js
+++ modules/circularNumbers.js
@@ -1,5 +1,18 @@
-const circularNumbers = (position, limit) => {
-
+const circularNumbers = (position, change, limit) => {
+  
+  let result = position + change;
+  console.log('result, position, change, limit', result, position, change, limit);
+  while (result > limit || result < 0) {
+    console.log(`  > while() result: `, result);
+    if (result > limit) {
+      result -= limit + 1;
+    }
+    if (result < 0) {
+      result += limit + 1;
+    }
+  }
+  console.log(`    >> returning :`, result);
+  return result;
 };
 
 module.exports = circularNumbers;
--- test/circularNumbersTest.js
+++ test/circularNumbersTest.js
@@ -1,11 +1,41 @@
-const assert = require('chai');
+const assert = require('chai').assert;
 const circularNumbers = require('../modules/circularNumbers');
 
 describe('circularNumbers(position, limit)', () => {
-  it('limit : 10, position 7 to 8 ', () => {
-    
-
-
-
+  it('limit : 10, +1, position 7 to 8', () => {
+    const test = 7;
+    const correct = 8;
+    const result = circularNumbers(test, 1, 10);
+    assert.strictEqual(result, correct);
+  });
+  it('limit : 10, -1, position 7 to 6', () => {
+    const test = 7;
+    const correct = 6;
+    const result = circularNumbers(test, -1, 10);
+    assert.strictEqual(result, correct);
+  });
+  it('limit : 10, 1, position 10 to 0', () => {
+    const test = 10;
+    const correct = 0;
+    const result = circularNumbers(test, 1, 10);
+    assert.strictEqual(result, correct);
+  });
+  it('limit : 10, -1, position 0 to 10', () => {
+    const test = 0;
+    const correct = 10;
+    const result = circularNumbers(test, -1, 10);
+    assert.strictEqual(result, correct);
+  });
+  it('limit : 10, 12, position 0 to 1', () => {
+    const test = 0;
+    const correct = 1;
+    const result = circularNumbers(test, 12, 10);
+    assert.strictEqual(result, correct);
+  });
+  it('limit : 117, -5, position 117 to 112', () => {
+    const test = 117;
+    const correct = 112;
+    const result = circularNumbers(test, -5, 117);
+    assert.strictEqual(result, correct);
   });
 });
